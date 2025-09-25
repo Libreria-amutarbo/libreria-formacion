b6e05c9f85363f7a7e6b54be79dea0c537f68043
Angela <angelahermosillad@gmail.com>
2025-09-25 11:50:30 +0200

    fix(input): fix enum and conditional

--- libs/dcx-ng-lib/src/lib/dcx-ng-components/dcx-ng-input/dcx-ng-input.component.ts
+++ libs/dcx-ng-lib/src/lib/dcx-ng-components/dcx-ng-input/dcx-ng-input.component.ts
@@ -1,9 +1,26 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
-export type InputType = 'text' | 'number' | 'email' | 'password' | 'date' | 'search' | 'tel' | 'url';
-export type InputSize = 's' | 'm' | 'l' | 'xl';
+export enum InputType {
+  TEXT = 'text',
+  NUMBER = 'number',
+  EMAIL = 'email',
+  PASSWORD = 'password',
+  DATE = 'date',
+  SEARCH = 'search',
+  TEL = 'tel',
+  URL = 'url'
+}
+
+export enum InputSize {
+  SMALL = 's',
+  MEDIUM = 'm',
+  LARGE = 'l',
+  EXTRA_LARGE = 'xl'
+}
 
 @Component({
   selector: 'dcx-ng-input',
@@ -12,11 +29,11 @@ export type InputSize = 's' | 'm' | 'l' | 'xl';
   templateUrl: './dcx-ng-input.component.html',
   styleUrls: ['./dcx-ng-input.component.scss'],
 })
-export class DcxNgInputComponent implements OnInit, OnChanges {
+export class DcxNgInputComponent implements OnInit, OnChanges, OnDestroy {
 
-  @Input() type: InputType = 'text';
+  @Input() type: InputType = InputType.TEXT;
   @Input() placeholder: string | null = null;
-  @Input() size: InputSize = 'm';
+  @Input() size: InputSize = InputSize.MEDIUM;
   @Input() disabled: boolean = false;
   @Input() required: boolean = false;
   @Input() label: string | null = null;
@@ -31,9 +48,15 @@ export class DcxNgInputComponent implements OnInit, OnChanges {
   inputControl: FormControl = new FormControl('');
   isFocused: boolean = false;
   inputId: string;
+  private destroy$ = new Subject<void>();
 
   constructor() {
     this.inputId = `dcx-input-${Math.random().toString(36).substr(2, 9)}`;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   ngOnInit() {
@@ -46,10 +69,10 @@ export class DcxNgInputComponent implements OnInit, OnChanges {
     if (this.required) {
       validators.push(Validators.required);
     }
-    if (this.type === 'email') {
+    if (this.type === InputType.EMAIL) {
       validators.push(Validators.email);
     }
-    if (this.type === 'number') {
+    if (this.type === InputType.NUMBER) {
       validators.push(Validators.pattern(/^-?\d*\.?\d*$/));
     }
 
@@ -61,9 +84,11 @@ export class DcxNgInputComponent implements OnInit, OnChanges {
   }
 
   private setupValueChanges() {
-    this.inputControl.valueChanges.subscribe(value => {
-      this.valueChange.emit(value);
-    });
+    this.inputControl.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(value => {
+        this.valueChange.emit(value);
+      });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -79,7 +104,7 @@ export class DcxNgInputComponent implements OnInit, OnChanges {
     if (this.inputControl.hasError('email')) {
       return 'Formato correo inválido';
     }
-    if (this.inputControl.hasError('pattern') && this.type === 'number') {
+    if (this.inputControl.hasError('pattern') && this.type === InputType.NUMBER) {
       return 'Formato numérico inválido';
     }
     return null;
--- src/app/pages/dcx-ng-page-input/dcx-ng-page-input.component.ts
+++ src/app/pages/dcx-ng-page-input/dcx-ng-page-input.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DcxNgInputComponent } from '@dcx-ng-components/dcx-ng-lib';
+import { InputSize, InputType } from '../../../../libs/dcx-ng-lib/src/lib/dcx-ng-components/dcx-ng-input/dcx-ng-input.component';
 
 @Component({
   selector: 'dcx-ng-dcx-ng-input',
@@ -10,8 +11,13 @@ import { DcxNgInputComponent } from '@dcx-ng-components/dcx-ng-lib';
   styleUrls: ['./dcx-ng-page-input.component.scss'],
 })
 export class DcxNgPageInputComponent {
-
-  showValue() {
-    console.log('Valor cambiado');
+  valor = '';
+  
+  // Exponemos los enums para uso en el template
+  readonly InputType = InputType;
+  readonly InputSize = InputSize;
+  
+  showValue(value: string | null) {
+    this.valor = value || '';
   }
 }
