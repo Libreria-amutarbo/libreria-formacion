97d8815fff461223ed799f52c9752d29708d3f64
AaronT498 <aaron.tomas-blasco@capgemini.com>
2025-11-28 11:26:30 +0100

    Scrum 1 dialog (#70)
    * feat(dialog): add DcxNgDialogComponent
    
    * fix(dialog): fix storybook demo
    
    * fix(dialog): correct UnStyled and Documentation
    
    * fix(dialog): pr comments
    
    * fix(dialog): revisión Salvador
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,6 +19,7 @@ import { DcxNgPageSliderComponent } from './pages/dcx-ng-page-slider.component/d
 import { DcxNgPageDropdownComponent } from './pages/dcx-ng-page-dropdown/dcx-ng-page-dropdown.component';
 import { DcxNgPageIconFieldComponent } from './pages/dcx-ng-page-iconField/dcx-ng-page-iconField.component';
 import { DcxNgPageListComponent } from './pages/dcx-ng-page-list/dcx-ng-page-list.component';
+import { DcxNgPageDialogComponent } from './pages/dcx-ng-page-dialog/dcx-ng-page-dialog.component';
 
 export const appRoutes: Route[] = [
   {
@@ -99,7 +100,10 @@ export const appRoutes: Route[] = [
   },
   {
     path: 'slider',
-    component: DcxNgPageSliderComponent
-  }
+    component: DcxNgPageSliderComponent,
+  },
+  {
+    path: 'dialog',
+    component: DcxNgPageDialogComponent,
+  },
 ];
-
