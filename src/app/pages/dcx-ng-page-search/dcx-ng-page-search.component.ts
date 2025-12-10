3a9b33f274d191da91559dada188c9a57763caa0
AlbHerna <alberto.hernandez-san-pedro@capgemini.com>
2025-12-10 09:43:05 +0100

    albherna - ADD new functionality in search page

--- src/app/pages/dcx-ng-page-search/dcx-ng-page-search.component.ts
+++ src/app/pages/dcx-ng-page-search/dcx-ng-page-search.component.ts
@@ -1,5 +1,12 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { DcxNgSearchComponent } from '@dcx-ng-components/dcx-ng-lib';
+import { 
+  DcxNgSearchComponent, 
+  SearchItem, 
+  generatePersonRows, 
+  generateUserRows,
+  PersonRow,
+  UserRow
+} from '@dcx-ng-components/dcx-ng-lib';
 
 @Component({
   selector: 'dcx-ng-page-search',
@@ -11,6 +18,28 @@ import { DcxNgSearchComponent } from '@dcx-ng-components/dcx-ng-lib';
 export class DcxNgPageSearchComponent {
   readonly lastSearch = signal<string>('');
   readonly searchResults = signal<string[]>([]);
+  readonly selectedItem = signal<SearchItem | null>(null);
+  readonly searchValue = signal<string>('');
+  
+  readonly minThreeCharsRegex = /^.{3,}$/;
+  
+  readonly onlyLettersRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*$/;
+  
+  readonly personItems = signal<SearchItem[]>(
+    generatePersonRows(20).map((person: PersonRow) => ({
+      id: person.id ?? 0,
+      label: `${person.name} (${person.country})`,
+      data: person
+    }))
+  );
+
+  readonly userItems = signal<SearchItem[]>(
+    generateUserRows(30).map((user: UserRow) => ({
+      id: user.id ?? 0,
+      label: `${user.name} - ${user.age} años - ${user.country}`,
+      data: user
+    }))
+  );
 
   onSearchChange(value: string): void {  }
 
@@ -26,5 +55,14 @@ export class DcxNgPageSearchComponent {
     } else {
       this.searchResults.set([]);
     }
+  }
+
+  onItemSelected(item: SearchItem): void {
+    this.selectedItem.set(item);
+    console.log('Item seleccionado:', item);
+  }
+
+  onSearchValueChange(value: string): void {
+    this.searchValue.set(value);
   }
 }
