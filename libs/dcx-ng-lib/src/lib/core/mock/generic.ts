39b34b53962ec6cb844903dc5ac72c49247623a2
amutarbo-cap <alberto.mut-arbona@capgemini.com>
2025-12-16 09:10:24 +0100

    feat/LC-47 Icon Grid

--- libs/dcx-ng-lib/src/lib/core/mock/generic.ts
+++ libs/dcx-ng-lib/src/lib/core/mock/generic.ts
@@ -1,4 +1,4 @@
-import { DcxLayout, DcxAlign, DcxSize } from '../interfaces';
+import { DcxLayout, DcxAlign, DcxSize, IconSpacing } from '../interfaces';
 
 export const LAYOUT_LIST: DcxLayout[] = ['vertical', 'horizontal'];
 
@@ -6,8 +6,12 @@ export const ALIGN_LIST: DcxAlign[] = ['start', 'center', 'end'];
 
 export const SIZE_LIST: DcxSize[] = ['s', 'm', 'l'];
 
+export const ICON_SPACING_LIST: IconSpacing[] = ['none', 'compact', 'spacious'];
+
 export const LAYOUT_DEFAULT: DcxLayout = 'vertical';
 
 export const ALIGN_DEFAULT: DcxAlign = 'center';
 
 export const SIZE_DEFAULT: DcxSize = 's';
+
+export const ICON_SPACING_DEFAULT: IconSpacing = 'none';
