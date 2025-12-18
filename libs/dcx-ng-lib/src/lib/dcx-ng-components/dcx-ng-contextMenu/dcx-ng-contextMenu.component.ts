8c1d22750f1c2754d1567846f43f5393ccde386b
AlbHerna <alberto.hernandez-san-pedro@capgemini.com>
2025-12-18 11:38:36 +0100

    albherna - FIX contextMenu con A20

--- libs/dcx-ng-lib/src/lib/dcx-ng-components/dcx-ng-contextMenu/dcx-ng-contextMenu.component.ts
+++ libs/dcx-ng-lib/src/lib/dcx-ng-components/dcx-ng-contextMenu/dcx-ng-contextMenu.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Renderer2, effect, input, output, signal } from '@angular/core';
 
 export interface Position {
   x: number;
@@ -14,37 +13,58 @@ export interface ContextMenuItem {
 @Component({
   selector: 'dcx-ng-context-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './dcx-ng-contextMenu.component.html',
   styleUrl: './dcx-ng-contextMenu.component.scss',
 })
 export class ContextMenuComponent {
-  @Input() items: ContextMenuItem[] = [];
-  @Input() visible = false;
-  @Input() position: Position = { x: 0, y: 0 };
+  readonly items = input.required<ContextMenuItem[]>();
+  readonly visible = input(false);
+  readonly position = input<Position>({ x: 0, y: 0 });
+  readonly closeOnClickOutside = input(true);
 
-  @Output() closed = new EventEmitter<void>();
+  readonly closed = output<void>();
 
-  constructor(private eRef: ElementRef) { }
+  private clickListener?: () => void;
 
-  @HostListener('document:click', ['$event'])
-  onClickOutside(event: MouseEvent) {
-    if (!this.eRef.nativeElement.contains(event.target)) {
-      this.hide();
-    }
-  }
+  constructor(
+    private readonly eRef: ElementRef,
+    private readonly renderer: Renderer2
+  ) {
+    effect(() => {
+      const isVisible = this.visible();
+      const shouldClose = this.closeOnClickOutside();
 
-  show(x: number, y: number) {
-    this.position = { x, y };
-    this.visible = true;
+      if (shouldClose && isVisible && !this.clickListener) {
+        setTimeout(() => {
+          this.clickListener = this.renderer.listen('document', 'click', (event: MouseEvent) => {
+            if (this.visible() && !this.eRef.nativeElement.contains(event.target)) {
+              this.hide();
+            }
+          });
+        }, 0);
+      } else if ((!isVisible || !shouldClose) && this.clickListener) {
+        this.clickListener();
+        this.clickListener = undefined;
+      }
+    });
+
+    effect((onCleanup) => {
+      onCleanup(() => {
+        if (this.clickListener) {
+          this.clickListener();
+          this.clickListener = undefined;
+        }
+      });
+    });
   }
 
   hide() {
-    this.visible = false;
     this.closed.emit();
   }
 
-  onItemClick(item: ContextMenuItem) {
+  onItemClick(item: ContextMenuItem, event: Event) {
+    event.stopPropagation();
     item.action();
     this.hide();
   }
