4a0a50e435d4933741dfeeb979f85f2ed12d1aa5
lga-cap <luis.garcia-arranz@capgemini.com>
2026-02-27 13:05:28 +0100

    Fix/accordion (#147)
    * minor change
    
    * accordion
    
    * minor change

--- src/app/pages/dcx-ng-page-accordion/dcx-ng-page-accordion.component.ts
+++ src/app/pages/dcx-ng-page-accordion/dcx-ng-page-accordion.component.ts
@@ -1,39 +1,103 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  TemplateRef,
+  ViewChild,
+  AfterViewInit,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { 
-  DcxNgAccordionComponent, 
+import {
+  DcxNgAccordionComponent,
+  DcxAccordionTransitionList,
+  DcxAccordionDefault,
+  DcxAccordionItemsWithIcon,
+  DcxAccordionItemsWithExpanded,
+  DcxAccordionItemsDisabled,
+  DcxAccordionLargeContent,
+  DcxAccordionItemsContentDisabled,
+  DcxNgButtonComponent,
+  DcxNgListComponent,
+  DcxNgInputComponent,
+  DcxInputType,
   DcxNgAccordionItem,
-  DcxAccordionMock,
-  ACCORDION_ITEMS_WITH_ICONS,
-  ACCORDION_ITEMS_WITH_EXPANDED,
-  ACCORDION_ITEMS_COMPLEX,
-  ACCORDION_ITEMS_LARGE_CONTENT
 } from '@dcx-ng-components/dcx-ng-lib';
 
 @Component({
   selector: 'dcx-ng-accordion-page',
   standalone: true,
-  imports: [CommonModule, DcxNgAccordionComponent],
+  imports: [
+    CommonModule,
+    DcxNgAccordionComponent,
+    DcxNgButtonComponent,
+    DcxNgListComponent,
+    DcxNgInputComponent,
+  ],
   templateUrl: './dcx-ng-page-accordion.component.html',
   styleUrl: './dcx-ng-page-accordion.component.scss',
 })
-export class DcxNgPageAccordionComponent {
-  defaultItems = DcxAccordionMock;
+export class DcxNgPageAccordionComponent implements AfterViewInit {
+  constructor(private cdr: ChangeDetectorRef) {}
+  defaultItems = DcxAccordionDefault;
 
-  withIconsItems = ACCORDION_ITEMS_WITH_ICONS;
+  withIcons = DcxAccordionItemsWithIcon;
 
-  withDisabledItems = ACCORDION_ITEMS_COMPLEX;
+  WithDisabledItems = DcxAccordionItemsDisabled;
+  WithContentDisabledItems = DcxAccordionItemsContentDisabled;
 
-  multipleOpenItems = DcxAccordionMock;
+  multipleOpenItems = DcxAccordionItemsWithExpanded;
 
-  defaultExpandedItems = ACCORDION_ITEMS_WITH_EXPANDED;
+  fastTransitionItems = DcxAccordionDefault;
 
-  fastTransitionItems = DcxAccordionMock;
+  slowTransitionItems = DcxAccordionDefault;
 
-  slowTransitionItems = DcxAccordionMock;
+  noTransitionItems = DcxAccordionDefault;
 
-  noTransitionItems = DcxAccordionMock;
+  largeContentItems = DcxAccordionLargeContent;
 
-  largeContentItems = ACCORDION_ITEMS_LARGE_CONTENT;
+  // templates grabbed via ViewChild
+  @ViewChild('buttonTemplate', { read: TemplateRef })
+  buttonTemplate!: TemplateRef<any>;
 
+  @ViewChild('formTemplate', { read: TemplateRef })
+  formTemplate!: TemplateRef<any>;
+
+  @ViewChild('listTemplate', { read: TemplateRef })
+  listTemplate!: TemplateRef<any>;
+
+  withComponents: DcxNgAccordionItem[] = [];
+
+  listItems = [
+    { text: 'Item 1' },
+    { text: 'Item 2' },
+    { text: 'Item 3' },
+    { text: 'Item 4' },
+  ];
+  DcxInputType = DcxInputType;
+
+  ngAfterViewInit(): void {
+    // avoid ExpressionChangedAfterItHasBeenChecked by running assignment later
+    Promise.resolve().then(() => {
+      this.withComponents = [
+        {
+          id: '1',
+          title: 'Interactive Buttons',
+          icon: 'hand-pointer',
+          contentTemplate: this.buttonTemplate,
+        },
+        {
+          id: '2',
+          title: 'Form Components',
+          icon: 'file-text',
+          contentTemplate: this.formTemplate,
+        },
+        {
+          id: '3',
+          title: 'Dynamic List',
+          icon: 'list',
+          contentTemplate: this.listTemplate,
+        },
+      ];
+      this.cdr.detectChanges();
+    });
+  }
 }
