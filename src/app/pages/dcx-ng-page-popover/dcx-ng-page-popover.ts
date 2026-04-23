9eee7183272d476313772e772c765bc25c0143e7
luisplaCapg <luis.planella-hernandez@capgemini.com>
2026-04-23 12:38:51 +0200

    feat: add all Popover examples to page

--- src/app/pages/dcx-ng-page-popover/dcx-ng-page-popover.ts
+++ src/app/pages/dcx-ng-page-popover/dcx-ng-page-popover.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
-import { DcxNgPopoverComponent, DcxNgButtonComponent } from '@dcx-ng-components/dcx-ng-lib';
+import {
+  DcxNgPopoverComponent,
+  DcxNgButtonComponent,
+  DcxNgListComponent,
+  DcxNgChipComponent,
+  DcxNgDividerComponent,
+} from '@dcx-ng-components/dcx-ng-lib';
 
 @Component({
   selector: 'app-dcx-ng-page-popover',
   standalone: true,
-  imports: [DcxNgPopoverComponent, DcxNgButtonComponent],
+  imports: [
+    DcxNgPopoverComponent,
+    DcxNgButtonComponent,
+    DcxNgListComponent,
+    DcxNgChipComponent,
+    DcxNgDividerComponent,
+  ],
   templateUrl: './dcx-ng-page-popover.html',
   styleUrl: './dcx-ng-page-popover.scss',
 })
-export class DcxNgPagePopoverComponent {}
+export class DcxNgPagePopoverComponent {
+  actionItems = [
+    { text: 'Edit', icon: 'pencil' },
+    { text: 'Duplicate', icon: 'copy' },
+    { divider: true },
+    { text: 'Delete', icon: 'trash' },
+  ];
+}
