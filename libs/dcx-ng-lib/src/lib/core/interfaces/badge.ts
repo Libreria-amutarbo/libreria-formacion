0846fe513c65056e4164252cd4cd54bfb842f616
luisplaCapg <luis.planella-hernandez@capgemini.com>
2026-05-19 12:52:13 +0200

    fix(LC-100): simplify interface types in DcxNgBadgeComponentInputs

--- libs/dcx-ng-lib/src/lib/core/interfaces/badge.ts
+++ libs/dcx-ng-lib/src/lib/core/interfaces/badge.ts
@@ -1,24 +1,8 @@
-export enum BadgeSeverity {
-  PRIMARY = 'primary',
-  SECONDARY = 'secondary',
-  SUCCESS = 'success',
-  INFO = 'info',
-  WARN = 'warn',
-  DANGER = 'danger',
-}
-
-export enum BadgeSize {
-  SM = 'sm',
-  MD = 'md',
-  LG = 'lg',
-  XL = 'xl',
-}
-
-export type BadgeSeverityType = `${BadgeSeverity}`;
-export type BadgeSizeType = `${BadgeSize}`;
+export type BadgeSeverityType = 'primary' | 'secondary' | 'success' | 'info' | 'warn' | 'danger';
+export type BadgeSizeType = 'sm' | 'md' | 'lg' | 'xl';
 
 export interface DcxNgBadgeComponentInputs {
-  value: ReturnType<() => string>;
-  severity: ReturnType<() => BadgeSeverityType>;
-  size: ReturnType<() => BadgeSizeType>;
+  value: string;
+  severity: BadgeSeverityType;
+  size: BadgeSizeType;
 }
