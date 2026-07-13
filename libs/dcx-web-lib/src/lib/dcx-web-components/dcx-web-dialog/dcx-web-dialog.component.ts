778aa74e3ea37424242d69680fb36b1a5fbe3cea
Daniel Breva <dabreal@alumni.uv.es>
2026-07-13 13:39:15 +0200

    refactor: extract dialog template into separate function

--- libs/dcx-web-lib/src/lib/dcx-web-components/dcx-web-dialog/dcx-web-dialog.component.ts
+++ libs/dcx-web-lib/src/lib/dcx-web-components/dcx-web-dialog/dcx-web-dialog.component.ts
@@ -5,6 +5,8 @@ import { customElement, property } from 'lit/decorators.js';
 import type { DialogPosition } from '../../core/interfaces/dialog';
 import { dcxWebDialogStyles } from './dcx-web-dialog.component.styles';
 import '../dcx-web-button/dcx-web-button.component';
+import { dcxWebDialogTemplate } from './dcx-web-dialog.component.html';
+
 
 @customElement('dcx-web-dialog')
 export class DcxWebDialog extends LitElement {
@@ -69,64 +71,7 @@ export class DcxWebDialog extends LitElement {
   }
 
   override render() {
-    if (!this.visible) {
-      return html``;
-    }
-
-    return html`
-      <div class="dcx-dialog-root">
-        <div
-          class="dcx-dialog__backdrop"
-          @pointerdown=${this._onBackdropClick}
-        ></div>
-
-        <div
-          class="${this._dialogClasses}"
-          role="dialog"
-          aria-modal="true"
-          aria-labelledby="${ifDefined(this.title ? this._dialogTitleId : undefined)}"
-        >
-          <div class="dcx-dialog__header">
-            ${
-              this.title
-                ? html`
-                    <h3
-                      id="${this._dialogTitleId}"
-                      class="dcx-dialog__title"
-                    >
-                      ${this.title}
-                    </h3>
-                  `
-                : null
-            }
-
-            ${
-              this.showClose
-                ? html`
-                    <dcx-web-button
-                      variant="icon-only"
-                      size="s"
-                      icon-name="x-lg"
-                      class="dcx-dialog__close"
-                      aria-label="Cerrar diálogo"
-                      @buttonClick=${this._close}
-                    >
-                    </dcx-web-button>
-                  `
-                : null
-            }
-          </div>
-
-          <div class="dcx-dialog__body">
-            <slot name="body"></slot>
-          </div>
-
-          <div class="dcx-dialog__footer">
-            <slot name="footer"></slot>
-          </div>
-        </div>
-      </div>
-    `;
+    return dcxWebDialogTemplate(this);
   }
 }
 
