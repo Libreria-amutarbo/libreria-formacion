6c6c5db5fdaf899fed9c81d2b3cdb88cf5793c64
Luis Garcia Arranz <luisgarciaarranz@MacBook-Air-de-Luis-2.local>
2026-07-14 09:49:59 +0200

    refactor iconField

--- libs/dcx-ng-lib/src/lib/dcx-ng-components/dcx-ng-iconField/dcx-ng-iconField.component.ts
+++ libs/dcx-ng-lib/src/lib/dcx-ng-components/dcx-ng-iconField/dcx-ng-iconField.component.ts
@@ -1,7 +1,6 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  computed,
   input,
   output,
 } from '@angular/core';
@@ -12,34 +11,28 @@ import {
   ICON_FIELD_ICON_SIZE,
 } from '../../core/defaults';
 import { DcxNgButtonComponent } from '../dcx-ng-button/dcx-ng-button.component';
+import { DcxNgIconComponent } from '../dcx-ng-icon/dcx-ng-icon.component';
 
 @Component({
   selector: 'dcx-ng-icon-field',
   standalone: true,
-  imports: [DcxNgButtonComponent],
+  imports: [DcxNgButtonComponent, DcxNgIconComponent],
   templateUrl: './dcx-ng-iconField.component.html',
   styleUrls: ['./dcx-ng-iconField.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Default,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DcxNgIconFieldComponent {
   iconName = input(ICON_FIELD_ICON_NAME);
   iconPosition = input<DcxIconFieldPosition>(ICON_FIELD_ICON_POSITION);
-
   iconSize = input<DcxSize>(ICON_FIELD_ICON_SIZE);
+  iconClickable = input(false);
+  iconAriaLabel = input<string | null>(null);
+  disabled = input(false);
 
   iconClick = output();
 
-  iconPositionChange = computed<string>(() => {
-    const mapped =
-      this.iconPosition() === 'left'
-        ? 'has-left'
-        : this.iconPosition() === 'right'
-          ? 'has-right'
-          : '';
-    return [mapped].filter(Boolean).join(' ');
-  });
-
-  onIconClick() {
+  onIconClick(): void {
+    if (this.disabled()) return;
     this.iconClick.emit();
   }
 }
